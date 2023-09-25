b206c6dda835fefb29971ccfade085b347a47d25
Raj <gamingmodmania@gmail.com>
2023-09-25 22:18:02 +0530

    Update about and add my resume

--- about.php
+++ about.php
@@ -10,6 +10,7 @@
     <link rel="canonical" href="https://www.wallpaper-access.com/about" />
     <title>About us || Wallpaper Access</title>
 </head>
+
 <body>
     <div class="page-container">
         <div class="page-container-inside">
@@ -22,39 +23,72 @@
                         <div class="psric-top">
                             <div class="heading-and-search-box">
                                 <h1>About us</h1>
-                                <?php include($_SERVER['DOCUMENT_ROOT'] . '/component/' . 'search-box.html') ?>
                             </div>
                             <main class="main-content">
-                                <section class="homepage-search-box" style="background-image : url('/assets/about-us.webp');background-position: center; background-repeat: no-repeat; background-size: cover;">
+                                <section class="homepage-search-box"
+                                    style="background-image : url('/assets/about-us.webp');background-position: center; background-repeat: no-repeat; background-size: cover;">
                                     <div class="homepage-search-box-inside">
                                     </div>
                                 </section>
                                 <section class="text-container">
+                                    <button type="button"
+                                        onclick="window.href.location = 'https://gallery.coastweb.online/RESUME.pdf'">My
+                                        Resume Link (Want to create your Website)</button>
                                     <h2>Developer & Designer</h2>
-                                    <p>Hi, My name is Ankit Yadav, I am Official Developer and Designer of this website. I am creative developer, Basically I can develop a website in every desgin. If anyone want to contact me to create your own website you can contact me to this E-mail <a href="mailto:arcreactor65@gmail.com">arcreactor65@gmail.com</a></p>
+                                    <p>Hi, My name is Ankit Yadav, I am Official Developer and Designer of this website.
+                                        I am creative developer, Basically I can develop a website in every desgin. If
+                                        anyone want to contact me to create your own website you can contact me to this
+                                        E-mail <a href="mailto:arcreactor65@gmail.com">arcreactor65@gmail.com</a></p>
                                     <h3>Work Experience</h3>
-                                    <p>I have 3-4 years of experience. I have good knowledge of HTML, CSS, JS, Developer Tools, Server Side Work, Figma, VS Code, APIs, a Little Bit knowledge of PHP & MySql. And always I try to explore my Skills.</p>
-                                    
+                                    <p>I have 3-4 years of experience. I have good knowledge of HTML, CSS, JS, Developer
+                                        Tools, Server Side Work, Figma, VS Code, APIs, a Little Bit knowledge of PHP &
+                                        MySql. And always I try to explore my Skills.</p>
+
                                     <!--<h3 class="highlight-text">Do you want to set up a website?</h3>-->
                                     <!--<img src="/assets/cover-create-your-own-website.webp" alt="create your own website cover image" style="width : 100%; max-width : 500px; display : block; margin: 10px auto;" />-->
                                     <p>
-                                        <a class="highlight-text" href="/contact">Contact me</a>, I provide you complete setup for website like Hosting, Domain, and Website. For first time you can get <span class="underline-highlight">One Year Hosting Free.</span>
+                                        <a class="highlight-text" href="/contact">Contact me</a>, I provide you complete
+                                        setup for website like Hosting, Domain, and Website. For first time you can get
+                                        <span class="underline-highlight">One Year Hosting Free.</span>
                                     </p>
-                                    <h3 class="highlight-heading">Think About <span class="highlight-italic-text" style="font-size: 12px;"></span></h3>
-                                   
+                                    <h3 class="highlight-heading">Think About <span class="highlight-italic-text"
+                                            style="font-size: 12px;"></span></h3>
+
                                     <h3>Selfish Man</h3>
-                                    <p style="font-size: 18px;">Now-a-days people become more selfish And it is right to become a selfish, Because if a man will be connected to everything around him, it is hard to leave them. So, if you want to get success in your life, you won't connect with everything around you. Only <span class="highlight-italic-text">meet them, spend time with them, and forget them.</span></p>    
+                                    <p style="font-size: 18px;">Now-a-days people become more selfish And it is right to
+                                        become a selfish, Because if a man will be connected to everything around him,
+                                        it is hard to leave them. So, if you want to get success in your life, you won't
+                                        connect with everything around you. Only <span
+                                            class="highlight-italic-text">meet them, spend time with them, and forget
+                                            them.</span></p>
                                     <h3>What is Books ?</h3>
-                                    <p style="font-size: 18px;">Books take away you beyond your think. Good Books are excites you to do something big in your life.</p>
+                                    <p style="font-size: 18px;">Books take away you beyond your think. Good Books are
+                                        excites you to do something big in your life.</p>
                                     <h3>More About Books ...</h3>
                                     <p style="font-size : 18px">
-                                        Everyone knows about books and also knows the main role of books in our entire life. When we are children, the books comes into our life as a giant virtual problem. Every child thinks, what the shit is this, child say to the mom, "Please save me, I don't want to study." Then Mom said that "Babu this is best for your good life and Books will help you in your entire life after me." And everyone knows that Moms never lie. Especially when she talks about your life.
+                                        Everyone knows about books and also knows the main role of books in our entire
+                                        life. When we are children, the books comes into our life as a giant virtual
+                                        problem. Every child thinks, what the shit is this, child say to the mom,
+                                        "Please save me, I don't want to study." Then Mom said that "Babu this is best
+                                        for your good life and Books will help you in your entire life after me." And
+                                        everyone knows that Moms never lie. Especially when she talks about your life.
                                         <br>
-                                        I don't say that a book is everything in your life, But in every field to do something big you need at least a good book that make you a well behaved man and really a human. Becuase in real-world beyond science you never be a man with an only body, you always be a man with your behaviour. Now-a-days when you look around you, you find that today's environment makes that child more aggresive, because I think that only way to make you a good person is reading good books because books, only good books never make a person aggresive, only make a human being.
+                                        I don't say that a book is everything in your life, But in every field to do
+                                        something big you need at least a good book that make you a well behaved man and
+                                        really a human. Becuase in real-world beyond science you never be a man with an
+                                        only body, you always be a man with your behaviour. Now-a-days when you look
+                                        around you, you find that today's environment makes that child more aggresive,
+                                        because I think that only way to make you a good person is reading good books
+                                        because books, only good books never make a person aggresive, only make a human
+                                        being.
                                     </p>
                                     <h3>Love</h3>
                                     <p style="font-size: 18px;">
-                                        Perhaps everyone has a love life in their life. Everyone knows that today's love is not real love. They only want a girl for their use in any way. Only a few people do really love her. But Now-a-days the environment and good think both are destroyed slowly. If you really want to know the meaning of real love, Filter your thoughts about girls.
+                                        Perhaps everyone has a love life in their life. Everyone knows that today's love
+                                        is not real love. They only want a girl for their use in any way. Only a few
+                                        people do really love her. But Now-a-days the environment and good think both
+                                        are destroyed slowly. If you really want to know the meaning of real love,
+                                        Filter your thoughts about girls.
                                     </p>
                                 </section>
                             </main>
@@ -64,10 +98,10 @@
                         <?php include($_SERVER['DOCUMENT_ROOT'] . '/component/' . 'footer.html'); ?>
                     </div>
                 </div>
-            </div>
+        </div>
         </section>
     </div>
-</div>
+    </div>
 </body>
 
 </html>
